6c452ff95ee666a648aca225f86fa3383683f1ca
ajacob <ajacob@powerlog.ch>
2025-09-19 15:18:06 +0200

    INTRANET LCS : correction easyadmin bug (Divider component)

--- src/Controller/Admin/AggridOptionCrudController.php
+++ src/Controller/Admin/AggridOptionCrudController.php
@@ -8,11 +8,7 @@ use App\Form\Type\JsonFieldType;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CodeEditorField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class AggridOptionCrudController extends AbstractCrudController
