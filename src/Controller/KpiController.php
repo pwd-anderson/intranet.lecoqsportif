259cea4f1c94943be3bbac00b8662533fe0d855b
ajacob <ajacob@powerlog.ch>
2025-10-21 10:06:09 +0200

    INTRANET LCS : KPI eCommerce

--- src/Controller/KpiController.php
+++ src/Controller/KpiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\Kpi;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class KpiController extends AbstractController
 {
     #[Route('/kpi/ecommerce', name: 'app_kpi_ecommerce')]
-    public function index(): Response
+    public function ecommerce(Kpi $kpi): Response
     {
-        return $this->render('kpi/ecommerce.html.twig');
+        $eshop = $kpi->getEshopData();
+        $products = $kpi->getTopProducts();
+        $commentaires = $kpi->getCommentaires();
+
+        return $this->render('kpi/ecommerce.html.twig', [
+            'eshop' => $eshop,
+            'products' => $products,
+            'commentaires' => $commentaires
+        ]);
     }
 }
