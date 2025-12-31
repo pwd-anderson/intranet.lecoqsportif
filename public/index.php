9ee758d5c4ee69dde436ce69de8c7ce9bebff82c
ajacob <ajacob@powerlog.ch>
2025-12-31 15:32:35 +0100

    INTRANET LCS : correction pour affichicher profiler bar en local ngrok

--- public/index.php
+++ public/index.php
@@ -2,6 +2,18 @@
 
 use App\Kernel;
 
+// --- FIX : Support HTTPS pour Ngrok / Dev uniquement ---
+// On ne force HTTPS que si on est sur ngrok et que le header est présent
+if (
+    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' &&
+    isset($_SERVER['HTTP_HOST']) &&
+    (str_contains($_SERVER['HTTP_HOST'], 'ngrok-free') || str_contains($_SERVER['HTTP_HOST'], 'localhost'))
+) {
+    $_SERVER['HTTPS'] = 'on';
+    $_SERVER['SERVER_PORT'] = 443;
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
