2d7dee66c8b6f9cc5e4e195a5fd03ceac8f486b4
ajacob <ajacob@powerlog.ch>
2026-06-22 18:01:11 +0200

    INTRANET LCS : BAcklog client -> ajouts nouvelles colonnes

--- src/Infrastructure/Sql/AgGrid/backlog_client_x3.sql
+++ src/Infrastructure/Sql/AgGrid/backlog_client_x3.sql
@@ -48,132 +48,152 @@ VALUES
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
  NULL,NULL,1,9,NULL,0,NULL,NULL),
 
+-- GENRE (nouveau)
+('backlog_client_x3_grid','GENRE','GENRE','string',120,1,'agTextColumnFilter',
+ JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
+ NULL,NULL,1,10,NULL,0,NULL,NULL),
+
+-- AGE (nouveau)
+('backlog_client_x3_grid','AGE','AGE','string',100,1,'agTextColumnFilter',
+ JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
+ NULL,NULL,1,11,NULL,0,NULL,NULL),
+
 -- 6
 ('backlog_client_x3_grid','ITMDES1_0','DESIGNATION','string',200,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,10,NULL,0,NULL,NULL),
+ NULL,NULL,1,12,NULL,0,NULL,NULL),
 
 -- 6
 ('backlog_client_x3_grid','DROPPE','DROPPÉ','string',200,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,11,NULL,0,NULL,NULL),
+ NULL,NULL,1,13,NULL,0,NULL,NULL),
 
 -- 7
 ('backlog_client_x3_grid','CLIENT_COMMANDE','CODE CLIENT CMD.','string',130,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,12,NULL,0,NULL,NULL),
+ NULL,NULL,1,14,NULL,0,NULL,NULL),
 
 -- 8
 ('backlog_client_x3_grid','NOM_CLIENT_COMMANDE','NOM CLIENT CMD.','string',190,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,13,NULL,0,NULL,NULL),
+ NULL,NULL,1,15,NULL,0,NULL,NULL),
 
 -- 7
 ('backlog_client_x3_grid','CLIENT','CODE CLIENT','string',130,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,14,NULL,0,NULL,NULL),
+ NULL,NULL,1,16,NULL,0,NULL,NULL),
 
 -- 8
 ('backlog_client_x3_grid','NOM_CLIENT','NOM CLIENT','string',180,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,15,NULL,0,NULL,NULL),
+ NULL,NULL,1,17,NULL,0,NULL,NULL),
 
 -- 9
 ('backlog_client_x3_grid','ADRESSE_LIVRAISON','ADRR. LIVRAISON','string',200,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,16,NULL,0,NULL,NULL),
+ NULL,NULL,1,18,NULL,0,NULL,NULL),
 
 -- 8
 ('backlog_client_x3_grid','VILLE','VILLE','string',180,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,17,NULL,0,NULL,NULL),
+ NULL,NULL,1,19,NULL,0,NULL,NULL),
 
 -- 9
 ('backlog_client_x3_grid','INDEPENDANT_GROUPMENT','NOM GROUPEMENT','string',200,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,18,NULL,0,NULL,NULL),
+ NULL,NULL,1,20,NULL,0,NULL,NULL),
 
 -- 10
 ('backlog_client_x3_grid','NUM_COMMANDE','N° COMMANDE','string',140,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,19,NULL,0,NULL,NULL),
+ NULL,NULL,1,21,NULL,0,NULL,NULL),
 
 -- 11
 ('backlog_client_x3_grid','DATE_COMMANDE','DATE COMMANDE','date',130,1,'agDateColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,20,NULL,0,NULL,NULL),
+ NULL,NULL,1,22,NULL,0,NULL,NULL),
 
 -- 12
 ('backlog_client_x3_grid','DATE_LIVRAISON','DATE LIVRAISON','date',130,1,'agDateColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,21,NULL,0,NULL,NULL),
+ NULL,NULL,1,23,NULL,0,NULL,NULL),
 
 -- 13
 ('backlog_client_x3_grid','REP1','REPRESENTANT 1','string',140,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,22,NULL,0,NULL,NULL),
+ NULL,NULL,1,24,NULL,0,NULL,NULL),
 
 -- 13
 ('backlog_client_x3_grid','REP2','REPRESENTANT 2','string',140,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,23,NULL,0,NULL,NULL),
+ NULL,NULL,1,25,NULL,0,NULL,NULL),
 
 -- 14
 ('backlog_client_x3_grid','QUANTITE','QUANTITE','integer',110,1,'agNumberColumnFilter',
  JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,24,NULL,0,'integerFormatter','integerComparator'),
+ NULL,NULL,1,26,NULL,0,'integerFormatter','integerComparator'),
 
 -- 15
 ('backlog_client_x3_grid','PRIX','PRIX DEVISE','decimal',130,1,'agNumberColumnFilter',
  JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,25,NULL,0,'decimalFormatter','decimalComparator'),
+ NULL,NULL,1,27,NULL,0,'decimalFormatter','decimalComparator'),
 
 -- 16
 ('backlog_client_x3_grid','CUR_0','DEVISE','string',100,1,'agTextColumnFilter',
  JSON_OBJECT('textAlign','left','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,26,NULL,0,NULL,NULL),
+ NULL,NULL,1,28,NULL,0,NULL,NULL),
+
+-- REMISE AUTO (nouveau)
+('backlog_client_x3_grid','REMISE_AUTO','REMISE AUTO','decimal',110,1,'agNumberColumnFilter',
+ JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
+ NULL,NULL,1,29,NULL,0,'decimalFormatter','decimalComparator'),
+
+-- REMISE MANU (nouveau)
+('backlog_client_x3_grid','REMISE_MANU','REMISE MANU','decimal',110,1,'agNumberColumnFilter',
+ JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
+ NULL,NULL,1,30,NULL,0,'decimalFormatter','decimalComparator'),
 
 -- 17
 ('backlog_client_x3_grid','PRIX_EUR','PRIX EUR','decimal',130,1,'agNumberColumnFilter',
  JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,27,NULL,0,'decimalFormatter','decimalComparator'),
+ NULL,NULL,1,31,NULL,0,'decimalFormatter','decimalComparator'),
 
 -- 18
 ('backlog_client_x3_grid','STOCK_INTERNE_WLOGM','STK INT. WLOGM','integer',110,1,'agNumberColumnFilter',
  JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,28,'bg-info',0,'integerFormatter','integerComparator'),
+ NULL,NULL,1,32,'bg-info',0,'integerFormatter','integerComparator'),
 
 -- 18
 ('backlog_client_x3_grid','STOCK_REEL_WLOGM','STK REEL WLOGM','integer',110,1,'agNumberColumnFilter',
  JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,29,'bg-info',0,'integerFormatter','integerComparator'),
+ NULL,NULL,1,33,'bg-info',0,'integerFormatter','integerComparator'),
 
 -- 18
 ('backlog_client_x3_grid','STOCK_INTERNE_WSFCN','STK INT. WSFCN','integer',110,1,'agNumberColumnFilter',
  JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,30,'bg-info-light',0,'integerFormatter','integerComparator'),
+ NULL,NULL,1,34,'bg-info-light',0,'integerFormatter','integerComparator'),
 
 -- 18
 ('backlog_client_x3_grid','STOCK_REEL_WSFCN','STK REEL WSFCN','integer',110,1,'agNumberColumnFilter',
  JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,31,'bg-info-light',0,'integerFormatter','integerComparator'),
+ NULL,NULL,1,35,'bg-info-light',0,'integerFormatter','integerComparator'),
 
 -- 18
 ('backlog_client_x3_grid','STOCK_INTERNE_WTAKH','STK INT. WTAKH','integer',110,1,'agNumberColumnFilter',
  JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,32,'bg-success-light',0,'integerFormatter','integerComparator'),
+ NULL,NULL,1,36,'bg-success-light',0,'integerFormatter','integerComparator'),
 
 -- 18
 ('backlog_client_x3_grid','STOCK_REEL_WTAKH','STK REEL WTAKH','integer',110,1,'agNumberColumnFilter',
  JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,33,'bg-success-light',0,'integerFormatter','integerComparator'),
+ NULL,NULL,1,37,'bg-success-light',0,'integerFormatter','integerComparator'),
 
 -- 18
 ('backlog_client_x3_grid','STOCK_INTERNE_WDTTH','STK INT. WDTTH','integer',110,1,'agNumberColumnFilter',
  JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,34,'bg-primary-light',0,'integerFormatter','integerComparator'),
+ NULL,NULL,1,38,'bg-primary-light',0,'integerFormatter','integerComparator'),
 
 -- 18
 ('backlog_client_x3_grid','STOCK_REEL_WDTTH','STK REEL WDTTH','integer',110,1,'agNumberColumnFilter',
  JSON_OBJECT('textAlign','right','borderRight','0.2px solid #CECECEFF','borderBottom','0.2px solid #CECECEFF'),
- NULL,NULL,1,35,'bg-primary-light',0,'integerFormatter','integerComparator');
+ NULL,NULL,1,39,'bg-primary-light',0,'integerFormatter','integerComparator');
