361fe480a1c17a0be8fe539e923b273ce1d536b4
ajacob <ajacob@powerlog.ch>
2026-09-10 16:43:23 +0200

    INTRANET LCS : Stock a terme segmentation produits X3 -> ajout du champ Model

--- src/Service/Stock.php
+++ src/Service/Stock.php
@@ -203,7 +203,7 @@ class Stock
         try {
             $query = "
                 SELECT [SITE], [DESCRIPTION_SITE], [FAMILLE], [DERNIERE_COLLECTION],
-                       [ARTICLE], [EAN], [DESCRIPTION_ARTICLE], [STATUT], GENRE, SEG_OFFRE,
+                       [ARTICLE], [MODEL], [EAN], [DESCRIPTION_ARTICLE], [STATUT], GENRE, SEG_OFFRE,
                        [LARGEUR], [LONGUEUR], [HAUTEUR], [UNITE], [POIDS],
                        [PRIX_MARCHE], [DEVISE_MARCHE], [PDM_MATERIAL_CODE], [PAYS_ORIGINE],
                        [STOCK_INTERNE],
